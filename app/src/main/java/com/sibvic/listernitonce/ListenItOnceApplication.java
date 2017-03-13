30676121905d791a2d79c9e026ca7f0ebbe9c625
Victor Tereschenko <sibvic@gmail.com>
2017-03-13 12:26:35 +0600

    Crash reports support.

---
+++ app/src/main/java/com/sibvic/listernitonce/ListenItOnceApplication.java
@@ -0,0 +1,27 @@
+package com.sibvic.listernitonce;
+
+import android.app.Application;
+import android.content.Context;
+
+import org.acra.*;
+import org.acra.annotation.*;
+
+/**
+ * Main application class
+ */
+
+@ReportsCrashes(
+        mailTo = "reports@yourdomain.com",
+        mode = ReportingInteractionMode.TOAST,
+        resToastText = R.string.crash_report_text
+)
+public class ListenItOnceApplication extends Application {
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+
+        // The following line triggers the initialization of ACRA
+        ACRA.init(this);
+    }
+}
